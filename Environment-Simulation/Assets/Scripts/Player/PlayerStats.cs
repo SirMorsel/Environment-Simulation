3769f7ff8710253657816db905ed75f8da273de9
SirMorsel <patrick.nibbia@gmail.com>
2022-02-14 14:49:30 +0100

    chore: clean up code

--- Environment-Simulation/Assets/Scripts/Player/PlayerStats.cs
+++ Environment-Simulation/Assets/Scripts/Player/PlayerStats.cs
@@ -4,31 +4,9 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] private float maxHealth = 250.0F;
     [SerializeField] private float walkSpeed = 5.0F;
     [SerializeField] private float runSpeed = 10.0F;
     [SerializeField] private float turnSpeed = 10.0F;
-
-    [SerializeField] private float meleeDamage = 20.0F;
-    [SerializeField] private float criticalMultiplier = 5.0F;
-    [SerializeField] private float attackInterval = 2.0F;
-
-    private float currentHealth;
-    // Start is called before the first frame update
-
-    private void Awake()
-    {
-        currentHealth = maxHealth;
-    }
-    void Start()
-    {
-
-    }
-
-    public float GetCurrentHealth()
-    {
-        return currentHealth;
-    }
 
     public float GetWalkSpeed()
     {
@@ -43,20 +21,5 @@ public class PlayerStats : MonoBehaviour
     public float GetTurnSpeed()
     {
         return turnSpeed;
-    }
-
-    public float GetMeleeDamage()
-    {
-        return meleeDamage;
-    }
-
-    public float GetCritMultiplier()
-    {
-        return criticalMultiplier;
-    }
-
-    public float GetAttackInterval()
-    {
-        return attackInterval;
     }
 }
